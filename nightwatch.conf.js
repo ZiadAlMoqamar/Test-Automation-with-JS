d539411e2036a90907060f213dc8d56f52813d5a
ZiadAlMoqamar <ziad.almoqamar@gmail.com>
2024-04-11 02:53:01 +0200

    Temporary removal of page obj path

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -17,7 +17,7 @@ module.exports = {
   src_folders: 'ui-tests',
 
   // See https://nightwatchjs.org/guide/concepts/page-object-model.html
-  page_objects_path: ['ui-tests/page-ogbjects'],
+  page_objects_path: [],
 
   // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
   custom_commands_path: [],
